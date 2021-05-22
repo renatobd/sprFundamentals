8e97c44d0842f3e410313d89dd8f507c8c278adc
Renato Damasceno <rbdrenato@me.com>
2021-05-22 19:03:39 -0300

    Criando projeto Spring com configuração XML

--- conference-spring /src/main/java/AppConfig.java
+++ conference-spring /src/main/java/AppConfig.java
@@ -4,22 +4,25 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
-
+    /*
     @Bean(name = "SpeakerService")
-    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public SpeakerService getSpeakerService(){
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
         return service;
     }
 
     @Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 }
--- conference-spring /src/main/java/Application.java
+++ conference-spring /src/main/java/Application.java
@@ -8,13 +8,13 @@ public class Application {
 
         //SepakerService service = new SepakerServiceImpl();
 
-        SpeakerService service = appContext.getBean("SpeakerService", SpeakerService.class);
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service);
 
         System.out.println(service.findAll().get(0).getFirstName()  + " " +  service.findAll().get(0).getLastName());
 
-        SpeakerService service2 = appContext.getBean("SpeakerService", SpeakerService.class);
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service2);
     }
